a3793135b4aca5db27d904c8f5d82b448b4d647e
ayun0505 <ss860530@gmail.com>
2025-12-26 16:48:38 +0800

    OrderItems框架先弄好

---
+++ PcShop/Areas/OrderItems/Controllers/OrderItemsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PcShop.Areas.Cart.Repositories;
+using PcShop.Areas.OrderItems.Repositories;
+
+namespace PcShop.Areas.OrderItems.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderItemsController : ControllerBase
+    {
+        private readonly IOrderItemsService _orderItemsService;
+        public OrderItemsController(IOrderItemsService orderItemsService)
+        {
+            _orderItemsService = orderItemsService;
+        }
+    }
+}
