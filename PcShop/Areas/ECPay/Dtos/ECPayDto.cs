69fe140ebe549f78141671ea47fae4355867d458
Ayun <ss860530@gmail.com>
2026-01-13 20:25:48 +0800

    綠界新增:超商門市選擇(一版

--- PcShop/Areas/ECPay/Dtos/ECPayDto.cs
+++ PcShop/Areas/ECPay/Dtos/ECPayDto.cs
@@ -12,11 +12,22 @@ namespace PcShop.Areas.ECPay.Dtos
         public string? ChoosePayment { get; set; }
     }
 
-    /// <summary>
+     /// <summary>
     /// 回傳給前端的付款資訊 (包含 HTML 表單)
     /// </summary>
     public class PaymentResultDto
     {
         public string HtmlForm { get; set; }
+    }
+
+    /// <summary>
+    /// 綠界超商選站點請求
+    /// </summary>
+    public class LogisticsMapRequestDto
+    {
+        public string LogisticsType { get; set; } = "CVS";
+        public string LogisticsSubType { get; set; } // FAMI, UNIMART, HILIFE, OKMART
+        public string IsCollection { get; set; } = "N"; // 是否代收貨款
+        public string? ExtraData { get; set; } // 額外資訊 (例如前端傳回來的暫存ID)
     }
 }
